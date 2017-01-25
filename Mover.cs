e436aa9a6a95d2067e316fd53f480fe2395487a5
Dokge <paulviossat@orange.fr>
2017-01-25 09:08:54 +0100

    Add files via upload

---
+++ Mover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour  // Used to make collectables move down when they appear. The choice not respect physics and apply a constant velocity to the object is for gameplay purpose
+{
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.up * -speed;
+    }
+}
+    
