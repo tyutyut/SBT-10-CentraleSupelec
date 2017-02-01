8d387e3cfcfed5c2b5f1c843325109cdb8f5cf6b
JulienMalle <julien.malle@student.ecp.fr>
2017-02-01 10:51:34 +0100

    Add files via upload

---
+++ Scripts/Collect.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collect : MonoBehaviour
+{
+
+    public int scoreValue;
+
+    private GameController gameController;
+
+    void Start ()  // The GameController is selected with this function, as it's needed for AddScore()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)  // Used to make items disappear when touching the player
+    {
+        if (other.CompareTag("Boundary"))
+        {
+            return;
+        }
+        Destroy(gameObject);
+        gameController.AddScore(scoreValue);  // Add the score value of the collectable to the player's score (see PlayerController.cs)
+    }
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Boundary boundary;
+
+    private Rigidbody rb;
+    private GameController gameController;
+
+    void Start ()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if (gameController == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate ()
+    {
+        if (!gameController.Paused())
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");  // moveHorizontal = +1 (or -1) if right (or left) arrow key is held down
+
+            Vector3 mouvement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+            rb.transform.Translate(mouvement * speed * Time.fixedDeltaTime);
+
+            rb.position = new Vector3  // Force the player to stay in the screen (values fixed in the Boundary class)
+            (
+                Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+                0.0f,
+                0.0f
+            );
+        }
+    }
+}
